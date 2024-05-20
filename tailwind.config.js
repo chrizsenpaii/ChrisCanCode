989d3f855428670f4759c279d82a11a2d1a94d01
chrizsenpaii <csaguirre7@gmail.com>
2024-05-20 19:48:17 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'HoneyDew' : '#F1FAEE',
+        'PhotoBlue' : '#A8DADC',
+        'redPantone': '#E63946',
+        'Corulean': '#457B9D',
+        'BerkeleyBlue': '#1D3557',
+        'Midnight': '#212121',
+        'Lmidnight': '#323232',
+        'Cyan': '#14FFEC',
+        'Teal': '#0D7377'
+
+      },
+    },
+  },
+  plugins: [],
+}
